58802cd242996ae0a83efeb3e82751aba9bc902f
rarm <rarm.jet@gmail.com>
2020-11-13 15:14:23 +1100

    rebase commit

---
+++ app/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using app.Models;
+
+namespace app.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var random = new Random();
+                var length = random.Next();
+                var squareRoot = Math.Round(Math.Sqrt(length));
+                var powerHalf = Math.Round(Math.Pow(length, 0.5));
+            }
+
+            Console.Write("Connection Requested" + "\n");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
